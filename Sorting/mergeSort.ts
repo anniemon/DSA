0f5270a3093d349b2de071ab57cafa96ddf14d5d
anniemon <seojung19@gmail.com>
2025-08-03 13:05:43 +0900

    Implement merge sort algorithm in TypeScript

--- Sorting/mergeSort.ts
+++ Sorting/mergeSort.ts
@@ -1,14 +1,14 @@
-let sorted = [];
-function mergeSort(arr, left, right) {
+let sorted:number[] = [];
+function mergeSort(arr: Array<number>, left:number, right:number) {
 	if (left < right) {
-		let mid = parseInt((left + right) / 2 );
+		let mid = Math.floor((left + right) / 2);
 		mergeSort(arr, left, mid);
 		mergeSort(arr, mid + 1, right);
 		merge(arr, left, mid, right);
 	}
 }
 
-function merge(arr, left, mid, right) {
+function merge(arr:Array<number>, left:number, mid:number, right:number) {
 	let i = left;
 	let j = mid + 1;
 	let k = left;
@@ -33,3 +33,10 @@ function merge(arr, left, mid, right) {
 		arr[x] = sorted[x];
 	}
 }
+
+/**
+ * Usage
+ * const arr = [1, 5, 3, 3, 4, 8, 6, 9, 2];
+ * mergeSort(arr, 0, arr.length - 1);
+ * console.log(arr);
+ */
