b05d4a26cf32fdefcae7228fc7c130602acd0a5e
anniemon <seojung19@gmail.com>
2025-09-03 09:32:51 +0900

    Boj 19939(Greedy)

---
+++ Greedy/boj_19939.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+let [n, k] = fs.readFileSync('/dev/stdin').toString().split(' ').map(Number);
+
+let sum = 0;
+for(let i = 1; i <= k; i++) {
+  sum += i;
+}
+if(sum > n) {
+  console.log(-1);
+} else {
+  n -= sum;
+  if(n % k === 0) {
+    console.log(k - 1);
+  } else {
+    console.log(k);
+  }
+}
